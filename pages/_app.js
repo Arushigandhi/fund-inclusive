0a5f1353947a93d3bed511a849f5adf4ff305d11
Arushigandhi <arushi.gandhi@gmail.com>
2022-12-29 13:29:53 +0530

    added head in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "antd/dist/antd.css";
 import "antd/dist/antd.variable.min.css";
 import { ConfigProvider } from "antd";
 import { AuthUserProvider } from "../context/AuthUserContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   ConfigProvider.config({ theme: { primaryColor: "#ff385c" } });
@@ -10,6 +11,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <ConfigProvider>
       <AuthUserProvider>
+        <Head>
+          <title>Fund Inc</title>
+          {/* <meta
+            name="description"
+            content="BugBase, a managed marketplace of ethical hackers that hosts crowdsourced bug bounty programs for companies, ensuring continuous testing and significantly reducing the chance of a cyberattack"
+          />
+          <meta
+            name="keywords"
+            content="bugbase, bugbounty, hacking, CTF, india, programs, hacktivity, bugs, cybersecurity, security, technology, pentesting, cybercrime, infosec, jira, integrations, reports, ethical hacking, marketplace, cyberattack"
+          /> */}
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </AuthUserProvider>
     </ConfigProvider>
